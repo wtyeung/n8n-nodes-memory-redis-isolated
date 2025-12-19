3c5093b9ab69cf79179fdd84aef035b314cf1f93
Tim Yeung <tim.yeung@hku.hk>
2025-12-19 13:34:44 +0800

    Add .gitignore and remove node_modules and dist from version control

--- dist/nodes/MemoryRedisIsolated/MemoryRedisIsolated.node.d.ts
+++
@@ -1,6 +0,0 @@
-import { type INodeType, type INodeTypeDescription, type ISupplyDataFunctions, type SupplyData } from 'n8n-workflow';
-export declare class MemoryRedisIsolated implements INodeType {
-    usableAsTool: boolean;
-    description: INodeTypeDescription;
-    supplyData(this: ISupplyDataFunctions, itemIndex: number): Promise<SupplyData>;
-}
